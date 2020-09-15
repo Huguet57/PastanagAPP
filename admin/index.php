3f6f62eb60c2ce214eeb1a690b3aa7a6c6d27d6a
Andreu Huguet <andreuhuguet@gmail.com>
2020-09-15 16:44:26 +0200

    Disjoin titles

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,10 @@
-<h1>Crea nova taula/Inserta valors</h1>
+<h1>Crea nova taula</h1>
 <form action="createtable.php" method="GET">
 	<label>Crear taula: <input type="text" name="dbname" placeholder="Nom de la base de dades" /></label>
 	<input type="submit" />
 </form>
 
+<h1>Inserta valors</h1>
 <form action="insert.php" method="POST" enctype="multipart/form-data">
 	<label>Insert CSV: <input type="file" name="csvname" /></label>
 	<input type="text" name="dbname" placeholder="Nom de la base de dades" value="<?=isset($_GET['dbname']) ? $_GET['dbname'] : ''?>" />
