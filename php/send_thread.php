fc1e8334f9355b1b24c070533b347bda0db490cd
Andreu Huguet <andreuhuguet@gmail.com>
2020-09-15 18:21:12 +0200

    Create send_thread.php

---
+++ php/send_thread.php
@@ -0,0 +1,15 @@
+<?php
+  require '../credentials.php';
+  require 'utils.php';
+
+  $credentials = new Credentials();
+
+  $victimid = $_POST["victim-id"];
+  $killerid = $_POST["killer-id"];
+  $msgcontent = $_POST["msg-content"];
+  
+  $template = "INSERT INTO messages VALUES (NULL, $killerid, $victimid, NULL, $msgcontent)";
+  if (!query($template)) die("An error ocurred." . $template);
+  
+  header("Location: http://pastanagapp2020.mygamesonline.org/main.php");
+?>
