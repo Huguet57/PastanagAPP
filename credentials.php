b6ea679a38529410699d5dd4381e0501cb35f16b
Andreu Huguet <andreuhuguet@gmail.com>
2020-09-15 16:09:30 +0200

    Create credentials.php

---
+++ credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+class Credentials {
+  public $servername = "fdb30.awardspace.net";
+  public $username = "3580259_pastanaga";
+  public $password = "CA-btEjC3,v{PV,%";
+  public $dbname = "pastanaga";
+};
+
+?>
